623ff87ad46a1d0f54df91ed83e89d69a0cb546c
Lucas <lcseferreira@hotmail.com>
2022-03-18 08:03:58 -0300

    Functions em JS

---
+++ Guia Estelar JavaScript/Functions/functions.js
@@ -0,0 +1,14 @@
+console.log("Aula sobre Functions!");
+
+// Ideia: criar um app de frases motivacionais
+// Fase 1 => declaração da função - function declaration
+function createPhrases() {
+    console.log("Estudar é muito bom!");
+    console.log("Estudar é muito bom!");
+    console.log("Estudar é muito bom!");
+} // Esse bloco de código é guardado em memória
+
+// Fase 2 => executar a função - execute, call, run, invoke 
+createPhrases();
+
+console.log("Fim do programa!");
