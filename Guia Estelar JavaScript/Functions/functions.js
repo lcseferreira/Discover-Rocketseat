a3b499187ff8d227946e0cb5f35631910c1e2455
Lucas <lcseferreira@hotmail.com>
2022-03-18 08:37:19 -0300

    callback function

--- Guia Estelar JavaScript/Functions/functions.js
+++ Guia Estelar JavaScript/Functions/functions.js
@@ -1,5 +1,20 @@
-// Arrow function
+// Callback function
 
-const sayMyName = () => {
-    console.log("Lucas");
+function sayMyName(aName) {
+    console.log("antes de executar a função");
+
+    aName();
+
+    function testCallback() {
+        console.log("dentro de outro callback");
+    }
+
+    testCallback();
 }
+
+// Callback é uma função que está sendo passada como parâmetro para outra função
+sayMyName(
+    () => { // Passando uma função como argumento
+        console.log("estou em uma callback");
+    }
+)
