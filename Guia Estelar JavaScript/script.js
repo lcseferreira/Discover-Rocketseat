a847804c9b3f884cb22c11eccdb27ac4558bab0c
Lucas <lcseferreira@hotmail.com>
2022-03-22 21:57:03 -0300

    Prototype __proto__

---
+++ Guia Estelar JavaScript/script.js
@@ -0,0 +1,17 @@
+/*
+    Prototype
+
+        -> JS é uma linguagem baseada em protótipos
+        -> __proto__
+*/
+
+
+console.log(true.__proto__);
+console.log("Lucas".charAt(2)); // Caracter na posição
+console.log("Lucas".endsWith('s')); // Termina com
+console.log("Lucas".replace('c', 'k')); // 
+console.log("Lucas".length);
+
+let myName = "Lucas Eduardo Ferreira da Rosa";
+
+console.log(myName.slice(0, 13));
