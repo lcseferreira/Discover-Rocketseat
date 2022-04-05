2853740050909c37bbc73d42726ec7d1e7410297
Lucas <lcseferreira@hotmail.com>
2022-04-05 20:58:20 -0300

    Grouping e Arithmetic operators

---
+++ Guia Estelar JavaScript/arithOperators.js
@@ -0,0 +1,36 @@
+/*
+
+    Operadores aritméticos
+
+*/
+
+// Multiplicação
+console.log(3 * 4);
+
+// Divisão
+console.log(26 / 4);
+
+// Soma
+console.log(3 + 4);
+
+// Subtração
+console.log(14 - 4);
+
+// Resto
+let remainder = 11 % 4;
+console.log(remainder);
+
+// Incremento
+let increment = 0;
+increment++;
+console.log(increment);
+console.log(++increment);
+
+// Decremento
+let decrement = 10;
+decrement--;
+console.log(decrement);
+console.log(--decrement);
+
+// Exponencial
+console.log(3 ** 3);
