585b5ff5ce12273ea4b0dfebde012680e2f30d2d
Lucas <lcseferreira@hotmail.com>
2022-04-05 21:44:25 -0300

    throw, try ... catch

---
+++ Guia Estelar JavaScript/switchcase.js
@@ -0,0 +1,32 @@
+/*
+
+    switch case
+
+*/
+
+switch (4) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Thursday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Tuesday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Error");
+        break;
+}
