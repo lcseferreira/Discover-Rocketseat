abc6db4d638a7d92d62b9ed975529ddc868abfe5
Lucas <lcseferreira@hotmail.com>
2022-04-05 21:07:22 -0300

    Operadores de atribuição

--- Guia Estelar JavaScript/comparisonOperators.js
+++ Guia Estelar JavaScript/comparisonOperators.js
@@ -9,3 +9,10 @@ let two = 2;
 
 console.log(one == 2);
 console.log(one != 2);
+
+let result = two > one ? "Yes" : "No";
+console.log(result);
+
+result = two < one ? "Yes" : "No";
+console.log(result);
+
