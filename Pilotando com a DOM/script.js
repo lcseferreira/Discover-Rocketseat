41022b5670c86af2103e8f5efa4516103cb891e0
Lucas <lcseferreira@hotmail.com>
2022-04-07 13:29:55 -0300

    querySelector

--- Pilotando com a DOM/script.js
+++ Pilotando com a DOM/script.js
@@ -3,9 +3,16 @@ const element1 = document.getElementById("mytitle");
 console.log(element1);
 
 // Pegando elementos pelo nome da classe
-const element2 = document.getElementsByClassName("myclass")[0];
-console.log(element2);
+const element2a = document.getElementsByClassName("myclass")[0];
+console.log(element2a);
+
+const element2b = document.getElementsByClassName("myclass")[1];
+console.log(element2b);
 
 // Pegando elementos pela tag
 const element3 = document.getElementsByTagName("p")[0];
 console.log(element3);
+
+// Pegando um elementos pela query selector
+const element4 = document.querySelector("p#queryslc");
+console.log(element4);
