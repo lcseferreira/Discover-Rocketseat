75ad5054e50a2f9f3da562683527795d35e7a62f
Lucas <lcseferreira@hotmail.com>
2022-04-07 13:27:00 -0300

    getElementsByTagName

--- Pilotando com a DOM/script.js
+++ Pilotando com a DOM/script.js
@@ -2,6 +2,10 @@
 const element1 = document.getElementById("mytitle");
 console.log(element1);
 
-// Pegando um elemento pelo nome da classe
+// Pegando elementos pelo nome da classe
 const element2 = document.getElementsByClassName("myclass")[0];
 console.log(element2);
+
+// Pegando elementos pela tag
+const element3 = document.getElementsByTagName("p")[0];
+console.log(element3);
