64937eea85781da0b01d4ee23349713e8e397735
Lucas <lcseferreira@hotmail.com>
2022-04-07 13:23:36 -0300

    Introdução ao DOM - getElementById e getElementsByClassName

---
+++ Pilotando com a DOM/script.js
@@ -0,0 +1,7 @@
+// Pegando um elemento pelo ID
+const element1 = document.getElementById("mytitle");
+console.log(element1);
+
+// Pegando um elemento pelo nome da classe
+const element2 = document.getElementsByClassName("myclass")[0];
+console.log(element2);
